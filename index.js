2cf6e347e59c2bc3af56e184e918fc48c1e5fada
Samuel Sml <veramirandasamuel6@gmail.com>
2024-03-08 13:32:42 -0500

    Get users that works in id company 5

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ db.once("open", function () {
   const app = express();
   app.use(express.json());
 
-  // GET EVERY USERS
+  //* GET ALL USERS
   app.get("/api/users", async (req, res) => {
     try {
       const users = await User.find();
@@ -43,7 +43,7 @@ db.once("open", function () {
     }
   });
 
-  // GET 10 USERS
+  //* GET 10 USERS
   app.get("/api/users/limit", async (req, res) => {
     try {
       const users = await User.find().limit(10);
@@ -63,7 +63,7 @@ db.once("open", function () {
     }
   });
 
-  // GET COMPANIES
+  //* GET COMPANIES
   app.get("/api/companies", async (req, res) => {
     try {
       const companies = await Company.find();
@@ -79,6 +79,24 @@ db.once("open", function () {
         data: error,
       });
       console.error("object not found", error);
+    }
+  });
+
+  //* GET USERS THAT WORKS IN COMPANY WITH ID 5
+  app.get("/api/users/5", async (req, res) => {
+    try {
+      const usersCompany5 = await User.find({ empresa_id: 5 });
+      res.json({
+        ok: true,
+        message: "Users works in id company 5 found",
+        data: usersCompany5,
+      });
+    } catch (error) {
+      res.json({
+        ok: false,
+        message: "Users not found",
+        data: error,
+      });
     }
   });
 
