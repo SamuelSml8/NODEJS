2c8cc9efdfc45c7a182801b6ab093cda511b859b
Samuel Sml <veramirandasamuel6@gmail.com>
2024-03-08 09:35:07 -0500

    Get all users with nodeJS and express

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require("express");
+const moongoose = require("mongoose");
 
-const app = express();
+moongoose.connect(
+  "mongodb+srv://samuel8:t4ImxkPNMXAhIrLt@users-nodejs.eyxgrp5.mongodb.net/"
+);
 
-app.get("/", (req, res) => {
-  res.send("Welcome");
-});
+const db = moongoose.connection;
 
-app.listen(3000, () => {
-  console.log("Puerto abierto (3000)");
+db.on("error", console.error.bind(console, "Connection error"));
+
+db.once("open", function () {
+  console.log("Connected to MongoDB");
+  // Model
+  userSchema = moongoose.Schema({
+    name: String,
+    last_name: String,
+  });
+  const User = moongoose.model("user", userSchema);
+
+  const app = express();
+  app.use(express.json());
+
+  app.get("/api/users", async (req, res) => {
+    try {
+      const users = await User.find();
+      console.log(users);
+      res.json(users);
+    } catch (error) {
+      console.error("object not found", error);
+    }
+  });
+  app.listen(3000, function () {
+    console.log("Server started");
+  });
 });
