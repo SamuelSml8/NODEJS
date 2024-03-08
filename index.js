eaad7266e5e13734c74b46eae8959130012df90e
Samuel Sml <veramirandasamuel6@gmail.com>
2024-03-08 13:01:49 -0500

    Get just 10 users

--- index.js
+++ index.js
@@ -25,11 +25,40 @@ db.once("open", function () {
     try {
       const users = await User.find();
       console.log(users);
-      res.json(users);
+      res.json({
+        "ok": true,
+        "message": "Users found",
+        "data": users
+      });
     } catch (error) {
+      res.json({
+        "ok": false,
+        "message": "Users not found",
+        "data": error
+      });
       console.error("object not found", error);
     }
   });
+
+
+  app.get("/api/users/limit", async (req, res) => {
+    try {
+      const users = await User.find().limit(10);
+      console.log(users);
+      res.json({
+        "ok": true,
+        "message": "Just 10 users found",
+        "data": users
+      });
+    } catch (error) {
+      res.json({
+        "ok": false,
+        "message": "Users not found",
+        "data": error
+      });
+      console.error("object not found", error);
+    }
+  })
   app.listen(3000, function () {
     console.log("Server started");
   });
